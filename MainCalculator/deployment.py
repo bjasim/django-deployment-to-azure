22ce65d6179d87da4795b74dddd3db69fc6fb6c1
Bakr <bakr22hj@gmail.com>
2024-03-23 23:52:19 -0400

    fix connestion string variable

--- MainCalculator/deployment.py
+++ MainCalculator/deployment.py
@@ -24,7 +24,7 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
 
