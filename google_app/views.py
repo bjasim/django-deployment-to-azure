311dad6bb8ec86a914579889eb9f616dba8f246c
Bakr <bakr22hj@gmail.com>
2024-03-23 22:47:52 -0400

    uncomment google functions

--- google_app/views.py
+++ google_app/views.py
@@ -99,21 +99,21 @@ def main():
     #DATABASE
 
     service_name = "services/9662-B51E-5089"  # CloudSQL 
-    # get_specs(endpoint_url, API_KEY, service_filter, desired_categories, output_file_path)
-    # get_prices()
+    get_specs(endpoint_url, API_KEY, service_filter, desired_categories, output_file_path)
+    get_prices()
     combine_json_data(output_file_path, output_file_path2,combined_info)
     #Firestore information
-    # fetch_save(API_KEY, 'F17B-412E-CB64',firestore_skus)
+    fetch_save(API_KEY, 'F17B-412E-CB64',firestore_skus)
     
     # COMPUTE
-    # computeinfo()
+    computeinfo()
     
     
     #Getting storage specs
-    # get_storage_specs(endpoint_url,API_KEY,service_filter_storage,output_file_Storage)
-    # retrieve_prices_from_json(output_file_Storage,API_KEY,storage_combined)
-    # fetch_save(API_KEY, '6F81-5844-456A',block_storage_skus) # Block storage, Persistant Disk
-    # fetch_save(API_KEY, 'D97E-AB26-5D95',filestore_skus) # Cloud File Store 
+    get_storage_specs(endpoint_url,API_KEY,service_filter_storage,output_file_Storage)
+    retrieve_prices_from_json(output_file_Storage,API_KEY,storage_combined)
+    fetch_save(API_KEY, '6F81-5844-456A',block_storage_skus) # Block storage, Persistant Disk
+    fetch_save(API_KEY, 'D97E-AB26-5D95',filestore_skus) # Cloud File Store 
     
     
     #NETWORKING
