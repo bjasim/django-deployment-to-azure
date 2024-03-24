4035727bffe663c7aa68ffb7726097ad542ffbd7
Bakr <bakr22hj@gmail.com>
2024-03-24 02:57:57 -0400

    add urs to aws_app with fix

--- MainCalculator/deployment.py
+++ MainCalculator/deployment.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ['SECRET']
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
-DEBUG = False
+DEBUG = True
 
 INSTALLED_APPS = [
     'django.contrib.admin',
